ce03e07d263add3fdf4f87287205708f0837ca74
Alexander Senetcky <41960163+asenetcky@users.noreply.github.com>
2025-05-13 15:31:27 +0000

    feat: quarto 1.7.31

--- .devcontainer/quarto-1.7.31-cli.sh
+++ .devcontainer/quarto-1.7.31-cli.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+
 apt-get update
 
 apt-get install -y --no-install-recommends wget ca-certificates
 
-wget -qO- https://quarto.org/docs/download/_download.json | grep -oP "(?<=\"download_url\":\s\")https.*amd64\.deb -O quarto.deb
+wget "https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.31/quarto-1.7.31-linux-amd64.deb" -O quarto.deb
+
 dpkg -i quarto.deb
+
 rm quarto.deb
 
 quarto check install
